34f36a112bae11ae0909c3d14b5a24286e20dca9
Paulina <mimka.pr@gmail.com>
2020-04-01 12:28:49 +0100

    base, header

--- js/script.js
+++ js/script.js
@@ -1,43 +1,9 @@
-const menuBtn = document.querySelector('.menu-btn');
-const burger = document.querySelector('.menu-btn-burger');
+const button = document.getElementsByClassName('main-header-button')[0];
+const navLinks = document.getElementsByClassName('nav-links')[0];
 
-let showMenu =false;
+button.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
 
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu(){
-    if(!showMenu){
-        burger.classList.add('open');
-        showMenu = true;
-    } else{
-        burger.classList.remove('open');
-        showMenu =false;
-    }
-    
-}
+});
 
 
-//     const navLinks = document.querySelectorAll('.nav-links li');
-
-
-
-
-//     burger.addEventListener('click', ()=>{
-//         nav.classList.toggle('nav-active');
-
-//         navLinks.forEach((link,index) => {
-//             if(link.style.animation){
-//                 link.style.animation = '';
-//             }else{
-//                 link.style.animation=`navLinkFade 0.5s ease forwards${index/5+0.5}s`;
-//             }
-//         });
-        
-//         burger.classList.toggle("toggle");
-//         burger.classList.toggle('open');
-//         nav-navLinks.fadeToggle('open');
-//     });
-
-// }
-
-// navSlide();
