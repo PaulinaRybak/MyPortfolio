76e32072bf27f9f80c54344744c803532a6e30a6
Paulina <mimka.pr@gmail.com>
2020-04-01 15:27:25 +0100

    hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const button = document.getElementsByClassName('main-header-button')[0];
 const navLinks = document.getElementsByClassName('nav-links')[0];
+const burger = document.querySelector('.main-header-button');
 
 button.addEventListener('click', () => {
     navLinks.classList.toggle('open');
+    burger.classList.toggle('toggle');
 
 });
+
 
 
